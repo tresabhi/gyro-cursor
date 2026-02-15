6a76853dffe38bdaddf9ca1831a9689e519b81a9
TrèsAbhi <43380238+tresabhi@users.noreply.github.com>
2026-02-15 16:25:56 -0600

    integration

--- app/src/main/java/com/tresabhi/gyrocursor/MainActivity.kt
+++ app/src/main/java/com/tresabhi/gyrocursor/MainActivity.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
@@ -53,13 +54,21 @@ fun GyroTracker(modifier: Modifier = Modifier) {
     var x by remember { mutableFloatStateOf(0f) }
     var y by remember { mutableFloatStateOf(0f) }
     var z by remember { mutableFloatStateOf(0f) }
+    var lastTimestamp by remember { mutableLongStateOf((0L)) }
 
     DisposableEffect(Unit) {
         val listener = object : SensorEventListener {
             override fun onSensorChanged(event: SensorEvent) {
-                x = event.values[0]
-                y = event.values[1]
-                z = event.values[2]
+                if (lastTimestamp == 0L) {
+                    lastTimestamp = event.timestamp
+                }
+
+                val dt = (event.timestamp - lastTimestamp) * 1e-9f;
+                lastTimestamp = event.timestamp
+
+                x += event.values[0] * dt
+                y += event.values[1] * dt
+                z += event.values[2] * dt
             }
 
             override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
@@ -78,8 +87,9 @@ fun GyroTracker(modifier: Modifier = Modifier) {
 
     Column(modifier = modifier) {
         Text("Gyroscope values:")
-        Text("X: $x")
-        Text("Y: $y")
-        Text("Z: $z")
+
+        Text("X: ${"%.2f".format(x)}")
+        Text("Y: ${"%.2f".format(y)}")
+        Text("Z: ${"%.2f".format(z)}")
     }
 }
